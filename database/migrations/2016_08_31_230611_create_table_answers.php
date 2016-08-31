6ae76b46f28a71331b2a642d67393142ba2f8286
Rurik Pinheiro <rurikspin@gmail.com>
2016-08-31 23:11:28 +0000

    create table answers

---
+++ database/migrations/2016_08_31_230611_create_table_answers.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAnswers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');
+            $table->integer('question_id')->unsigned();
+            $table->index('question_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('answers');
+    }
+}
